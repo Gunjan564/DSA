11a9fb196da6bf9b2e828590b766557fbcf1e76d
Gunjan <>
2024-07-23 15:35:09 +0530

    hello

---
+++ hashing/hashing.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int size,arr[size];
+    cin>>size;
+    for(int i=0;i<size;i++)
+    {
+        cin>>arr[i];
+    }
+    int t;
+    cin>>t;
+    
+return 0;
+}
