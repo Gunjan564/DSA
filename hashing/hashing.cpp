2327dbcdc3de079994452509b47312ea7e22c029
Gunjan <>
2024-07-24 00:56:09 +0530

    hello

--- hashing/hashing.cpp
+++ hashing/hashing.cpp
@@ -1,15 +1,28 @@
 #include<iostream>
+#include <algorithm>
 using namespace std;
 int main()
 {
-    int size,arr[size];
+    int size;
     cin>>size;
+    int arr[size];
     for(int i=0;i<size;i++)
     {
         cin>>arr[i];
     }
-    int t;
+    int* maxElement =max_element(arr, arr + size);
+    int hash_array[*maxElement+1]={0};
+    for(int i=0;i<size;i++)
+    {        
+        hash_array[arr[i]]+=1;
+    }
+    int t,test;
     cin>>t;
-    
+    while(t--)
+    {
+        
+        cin>>test;
+        cout<<hash_array[test]<<endl;        
+    }
 return 0;
 }
