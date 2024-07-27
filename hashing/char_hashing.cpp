d2a6d82a0d8789ef89139c709974234702845221
Gunjan <>
2024-07-27 12:11:04 +0530

    hello

--- hashing/char_hashing.cpp
+++ hashing/char_hashing.cpp
@@ -4,18 +4,34 @@ int main()
 {
     string str;
     getline(cin,str);
-    int hash_array[27]={0};
+    // /----character hashing for lower case letters(same for uppercase letter)
+    // int hash_array[27]={0};
+    // for(int i=0;i<str.length();i++)
+    // {
+    //     hash_array[(str[i])-'a']++;
+    // }
+    // int t;
+    // char test_character;
+    // cin>>t;
+    // while(t>0)
+    // {
+    //     cin>>test_character;
+    //     cout<<hash_array[(test_character)-'a']<<endl;
+    //     t--;
+    // }
+    // charater hashing for other letters or both uppercase and lowercase
+    int hash_array[256]={0};
     for(int i=0;i<str.length();i++)
     {
-        hash_array[(str[i])-'a']++;
+        hash_array[str[i]]++;
     }
     int t;
-    char test_character;
     cin>>t;
+    char test_character;
     while(t>0)
     {
         cin>>test_character;
-        cout<<hash_array[(test_character)-'a']<<endl;
+        cout<<hash_array[test_character]<<endl;
         t--;
     }
 return 0;
